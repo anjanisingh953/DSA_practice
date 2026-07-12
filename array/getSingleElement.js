c5ab6b695a46e7e7bf782c85f2f8d35b4e3e4af4
Anjani Singh <anjanisingh953@gmail.com>
2026-07-12 06:28:48 +0530

    find single occurence of an element problem resolved

--- array/getSingleElement.js
+++ array/getSingleElement.js
@@ -2,8 +2,8 @@ const arr = [1,1,2,3,3,4,4];
 
 
 
-function (arr){
-    let xorr =0getSingleElement;
+function getSingleElement(arr){
+    let xorr = 0;
 
     for(let i=0; i<arr.length; i++){
         xorr = xorr ^ arr[i];
