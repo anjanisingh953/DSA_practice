8a7c9cb8722895cc839224208d7065487aa52f55
Anjani Singh <anjanisingh953@gmail.com>
2026-08-12 17:28:23 +0530

    binary search resolved using iterative and recursive approach

--- searching/binary_search.js
+++ searching/binary_search.js
@@ -1,6 +1,28 @@
 const  arr = [-1,0,3,5,9,12];
 const target = 9;
 
+// //binary search using recursive approach
+// function binarySearch(arr, target) {
+//     let n = arr.length;
+//     let low = 0;
+//     let high = n-1;
+
+//     while(low <= high){
+//         let mid = Math.floor((low+high)/2);
+
+//         if(arr[mid] == target) return mid;
+//         else if(target < arr[mid]) high = mid -1;
+//         else low = mid + 1;
+//     }
+
+//     return -1;
+// }
+
+// const result = binarySearch(arr,target);
+// console.log('result >>>', result);
+
+
+//binary search using recursive approach
 function binarySearch(arr, low, high, target) {
     if (low > high) return -1;
 
@@ -23,3 +45,5 @@ var search = function(arr, target) {
 
 const result = search(arr,target);
 console.log('result >>>', result);
+
+
